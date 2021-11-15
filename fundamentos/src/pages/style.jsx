874ea9a0cfd010db7f0176697690c08df5b405e3
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-15 12:53:51 -0500

    Module CSS

---
+++ fundamentos/src/pages/style.jsx
@@ -0,0 +1,9 @@
+import styles from './Style.module.css'
+
+export default function Style() {
+    return (
+        <div className={styles.blueviolet}>
+            <h1>Style using CSS modules</h1>
+        </div>
+    )
+}
